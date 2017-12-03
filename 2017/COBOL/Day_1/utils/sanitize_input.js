b5f3e82d912684bbfe525d96e25604260928ff70
Alex Chrostowski <alex@alexchro.info>
2017-12-02 20:30:29 -0500

    Update Day 1 data files

--- 2017/COBOL/Day_1/utils/sanitize_input.js
+++ 2017/COBOL/Day_1/utils/sanitize_input.js
@@ -1,18 +1,12 @@
 /* Probably not the best way to do this but... oh well. */
-
 const util = require('util');
 const fs = require('fs');
 
-const inpFile = './DATA1';
-const outFile = './INPUT1.DAT';
+const inpFile = './DATA';
+const outFile = './INPUT.DAT';
 
-(function() {
-    try {
-        const data = fs.readFileSync(inpFile, { encoding: 'utf-8' }); 
-        for (let i = 0; i < data.length; i++) {
-            fs.appendFileSync(outFile, data.charAt(i) + '\n', { encoding: 'utf-8' });
-        }      
-    } catch(err) {
-        console.log(err);
-    }
-})();
+/* Format input for COBOL program */
+const data = fs.readFileSync(inpFile, { encoding: 'utf-8' }); 
+for (let i = 0; i < data.length; i++) {
+    fs.appendFileSync(outFile, data.charAt(i) + '\n', { encoding: 'utf-8' });
+}      
