9027d3e199555d0342f73cba0dd1292b9c5e0b94
Alex Chrostowski <alex@alexchro.info>
2017-12-06 20:50:16 -0500

    Add solution to Day 6 chellenge 2

---
+++ 2017/Javascript/Day_6/Day6_2.js
@@ -0,0 +1,31 @@
+const fs = require("fs");
+
+const inpFile = "./input";
+let blocks = fs.readFileSync(inpFile).toString().split(" ").map(Number);
+
+/* Identify first state */
+let bankState = blocks.join('');
+
+/* Identify remaining states */
+let states = {};
+let numSteps = 0;
+while (states[bankState] != 2) {
+    states[bankState] = ++states[bankState] || 1;
+
+    /* Find index of largest num blocks, get num blocks and set val to 0 */
+    let largestNum = Math.max(...blocks);
+    let indexLargestNum = blocks.indexOf(largestNum);
+    blocks[indexLargestNum] = 0;
+
+    /* Distribute blocks  */
+    let index = indexLargestNum + 1;
+    while (largestNum > 0) {
+        blocks[(index++) % blocks.length]++;
+        largestNum--;
+    }
+
+    bankState = blocks.join('');
+    numSteps++;
+}
+
+console.log(numSteps);
